c2b3cc1ce36a10d67a14cb64883719344af61cc2
Матвеев Сергей <zashms@list.ru>
2023-10-30 19:39:01 +0300

    created environment module start app

---
+++ environment/src/main/java/ru/socialnet/team43/EnvironmentApplication.java
@@ -0,0 +1,11 @@
+package ru.socialnet.team43;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvironmentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EnvironmentApplication.class, args);
+    }
+}
