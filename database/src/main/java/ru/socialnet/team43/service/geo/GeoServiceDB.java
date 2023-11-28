be0a8174842391c2e7f96b0677985916b16b73ff
Елена Янковская <elena.parokhina@gmail.com>
2023-11-28 12:36:10 +0300

    #15 building project ed.3

--- database/src/main/java/ru/socialnet/team43/service/geo/GeoServiceDB.java
+++
@@ -1,9 +0,0 @@
-package ru.socialnet.team43.service.geo;
-
-import ru.socialnet.team43.dto.geo.CountryDto;
-
-import java.util.List;
-
-public interface GeoServiceDB {
-    List<CountryDto> getCountries();
-}
--- profile/src/main/java/ru/socialnet/team43/service/GeoService.java
+++
@@ -1,9 +0,0 @@
-package ru.socialnet.team43.service;
-
-import ru.socialnet.team43.dto.geo.CountryDto;
-
-import java.util.List;
-
-public interface GeoService {
-    List<CountryDto> getCountry();
-}
