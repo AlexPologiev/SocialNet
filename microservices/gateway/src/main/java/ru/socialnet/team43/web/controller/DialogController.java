92ba78652391962cdbee9452e94193c19bbf9c43
Alexandr Polozhiev <A_pologiev@mail.ru>
2023-12-10 12:19:25 +0300

    create countsMainPage

---
+++ microservices/gateway/src/main/java/ru/socialnet/team43/web/controller/DialogController.java
@@ -0,0 +1,21 @@
+package ru.socialnet.team43.web.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.socialnet.team43.dto.UnreadCountDto;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/dialogs")
+public class DialogController {
+
+    @GetMapping("/count")
+    public ResponseEntity<UnreadCountDto> getCountUnreadDialogs(){
+        log.info("getCountUnreadDialogs");
+        return ResponseEntity.ok(new UnreadCountDto(0));
+    }
+}
