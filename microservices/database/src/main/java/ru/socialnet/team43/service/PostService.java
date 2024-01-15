1f0c5c9c5794eef8343f41bbd252cca8d29041a7
Матвеев Сергей <zashms@list.ru>
2024-01-15 08:57:31 +0300

    Получение тегов

--- microservices/database/src/main/java/ru/socialnet/team43/service/PostService.java
+++ microservices/database/src/main/java/ru/socialnet/team43/service/PostService.java
@@ -6,28 +6,33 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import ru.socialnet.team43.dto.PostDto;
 import ru.socialnet.team43.repository.PostRepository;
+import ru.socialnet.team43.repository.TagRepository;
+import ru.socialnet.team43.repository.Post2TagRepository;
 import ru.socialnet.team43.repository.mapper.PostDtoPostRecordMapper;
 
 import java.time.OffsetDateTime;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Component
 public class PostService {
 
-    private PostRepository repository;
+    private PostRepository postRepository;
+    private TagRepository tagRepository;
+    private Post2TagRepository post2TagRepository;
     private PostDtoPostRecordMapper mapper;
 
     public List<PostDto> getAll(List<Long> ids, List<Long> accountIds, List<Long> blockedIds,
                                 String author, String text, Boolean withFriends,
                                 Boolean isBlocked, Boolean isDeleted, OffsetDateTime dateFrom,
                                 OffsetDateTime dateTo, String sort) {
-        return repository.getAll(ids, accountIds, blockedIds, author, text, withFriends, isBlocked, isDeleted
+        return postRepository.getAll(ids, accountIds, blockedIds, author, text, withFriends, isBlocked, isDeleted
                 ,dateFrom, dateTo, sort);
     }
 
     public ResponseEntity<PostDto> getPostById(Long id) {
-        Optional<PostRecord> optionalRecord = repository.getPostById(id);
+        Optional<PostRecord> optionalRecord = postRepository.getPostById(id);
         Optional<PostDto> postDto = optionalRecord.map(mapper::postRecordToPostDto);
         if (postDto.isPresent()) {
             PostDto post = postDto.get();
@@ -38,47 +43,54 @@ public class PostService {
 
     public PostDto addNewPost(PostDto postDto) {
         PostRecord postRecord = mapper.postDtoToPostRecordForCreate(postDto);
-        boolean isQueued = postRecord.getPublishDate() != null;
-        Optional<PostRecord> post = repository.addNewPost(postRecord);
-        if (post.isPresent() && isQueued) {
-            addQueuedPost(post.get());
-        }
-        if (post.isPresent()) {
+        Long postId = postRepository.addNewPost(postRecord);
+        if (Objects.equals(postRecord.getType(), "QUEUED")) {
+            addQueuedPost(postId, postDto);
+        } else if (postId != 0L) {
+            List<Long> tagsIds = getTagsIds(postDto);
+            post2TagRepository.addNewRecords(postId, tagsIds);
             return postDto;
-        } else {
-            return null;
         }
+        return null;
     }
 
     public ResponseEntity<Long> editPost(PostDto postDto) {
-        if (repository.getPostById(postDto.getId()).isEmpty()) {
+        if (postRepository.getPostById(postDto.getId()).isEmpty()) {
             return ResponseEntity.badRequest().build();
         } else {
             PostRecord postRecord = mapper.postDtoToPostRecordForEdit(postDto);
-            repository.editPost(postRecord);
+            postRepository.editPost(postRecord);
             return ResponseEntity.ok().body(postRecord.getId());
         }
     }
 
     public ResponseEntity<Long> deletePost(Long id) {
-        if (repository.getPostById(id).isPresent()) {
-            repository.deletePostById(id);
+        if (postRepository.getPostById(id).isPresent()) {
+            postRepository.deletePostById(id);
             return ResponseEntity.ok().body(id);
         } else {
             return ResponseEntity.badRequest().build();
         }
     }
 
-    public void addQueuedPost(PostRecord postRecord) {
-        Date date = Date.from(postRecord.getPublishDate().toInstant());
+    public void addQueuedPost(Long postId, PostDto postDto) {
+        Date date = Date.from(postDto.getPublishDate().toInstant());
         Timer timer = new Timer();
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
-                repository.publishQueuedPost(postRecord);
+                postRepository.publishQueuedPost(postId);
+                List<Long> tagsIds = getTagsIds(postDto);
+                post2TagRepository.addNewRecords(postId, tagsIds);
                 timer.cancel();
             }
         }, date);
+    }
+
+    public List<Long> getTagsIds(PostDto postDto) {
+        return postDto.getTags().stream()
+                .map(tag -> tagRepository.addNewTag(tag.getName()))
+                .collect(Collectors.toList());
     }
 
 }
